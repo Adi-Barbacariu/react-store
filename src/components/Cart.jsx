02d5f048260f017bf4a749452c0ff3e9d52592b8
Adi Barbacariu <barbacariu.adi@gmail.com>
2020-09-24 18:30:24 +0300

    built modal for checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,50 +3,92 @@ import React from "react";
 import Item from "./Item";
 import { WithContext } from "./WithContext";
 
-function Cart(props) {
-  const { cartItems } = props;
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let totalPrice = 0;
-  let totalItems = 0;
-
-  if (cartItems.length === 0) {
-    return (
-      <div className="cart">
-        <p className="cart__title">Cart</p>
-        <h1>No items in cart.</h1>
-      </div>
-    );
+    this.state = { modal: false };
   }
 
-  return (
-    <div className="cart">
-      <p className="cart__title">Cart</p>
-      <div className="cart__container">
-        <div className="cart__items">
-          {cartItems.map((current) => {
-            totalPrice += current.quantity * current.price;
-            totalItems += current.quantity;
+  handleCheckout = () => {
+    this.setState((state) => {
+      return {
+        modal: !state.modal,
+      };
+    });
+  };
 
-            return (
-              <Item
-                key={current.id}
-                id={current.id}
-                name={current.name}
-                price={current.price}
-                quantity={current.quantity}
-              />
-            );
-          })}
+  componentDidMount() {
+    document.addEventListener("click", (event) => {
+      if (
+        this.state.modal &&
+        event.target.classList.contains("modal__wrapper")
+      ) {
+        this.handleCheckout();
+      }
+    });
+  }
+
+  render() {
+    const { cartItems } = this.props;
+
+    let totalPrice = 0;
+    let totalItems = 0;
+
+    if (cartItems.length === 0) {
+      return (
+        <div className="cart">
+          <p className="cart__title">Cart</p>
+          <h1>No items in cart.</h1>
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <div className="cart">
+          <p className="cart__title">Cart</p>
+
+          <div className="cart__container">
+            <div className="cart__items">
+              {cartItems.map((current) => {
+                totalPrice += current.quantity * current.price;
+                totalItems += current.quantity;
+
+                return (
+                  <Item
+                    key={current.id}
+                    id={current.id}
+                    name={current.name}
+                    price={current.price}
+                    quantity={current.quantity}
+                  />
+                );
+              })}
+            </div>
+
+            <div className="cart__check">
+              <p className="cart__total-items">Items: {totalItems}</p>
+              <p className="cart__total-price">Total: ${totalPrice}</p>
+              <button
+                onClick={this.handleCheckout}
+                className="cart__checkout-btn"
+              >
+                CHECKOUT
+              </button>
+            </div>
+          </div>
         </div>
 
-        <div className="cart__check">
-          <p className="cart__total-items">Items: {totalItems}</p>
-          <p className="cart__total-price">Total: ${totalPrice}</p>
-          <button className="cart__checkout-btn">CHECKOUT</button>
+        <div className={`modal__wrapper ${this.state.modal ? "" : "hide"}`}>
+          <div className="modal-checkout">
+            <h2 className="modal__title">Checkout</h2>
+            <button className="modal__btn">BUY</button>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      </>
+    );
+  }
 }
 
 export default WithContext(Cart);
